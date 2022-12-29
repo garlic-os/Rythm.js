2ce597b33464ba88fa1ba54d33bda1e0afc6360e
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2022-12-29 04:49:24 -0600

    Bug fixes and performance improvements
    - Improved beat detection algorithm
    - Combined files as project got smaller
    - Added debug readout that "graph" the beat detection value

--- src/analyse.js
+++ src/analyse.js
@@ -1,16 +1,33 @@
-// TODO: What are these?
 const STARTING_SCALE = 0;
 const PULSE_RATIO = 1;
 const MAX_HISTORY_SIZE = 100;
+const START_VALUE = 0;  // Index of the first frequency to average
+const NB_VALUE = 3;  // Number of frequencies to average to the right of START_VALUE
+const THESHOLD_LOW = 0.5;
+const THESHOLD_HIGH = 0.7;
 
 const hzHistory = [];
+let lastLoudness = 0;
+let lastBeatTime = new Date().getTime();
 
 
 /**
+ * Bias middling values toward 0 and 1 with the sigmoid function.
+ * So then because thou art lukewarm, and neither cold nor hot, I will spew thee
+ * out of my mouth.
+ * @param {number} x - The value to bias
+ */
+function sigmoid(x) {
+	return 1 / (1 + Math.exp((-30 * x) + 15));
+}
+
+
+/**
+ * @param {number[]} frequencies - A Wallpaper Engine audio frame
  * @param {number} index - The index of the frequency to get the ratio of
  * @returns {number} The ratio of the frequency to the maximum value in the history
  */
-function getFrequencyRatio(index) {
+function getFrequencyRatio(frequencies, index) {
 	let min = 1;
 	let max = 0;
 	for (const value of hzHistory[index]) {
@@ -25,6 +42,32 @@ function getFrequencyRatio(index) {
 	const actualValue = frequencies[index] - min;
 	const percentage = scale === 0 ? 0 : actualValue / scale;
 	return STARTING_SCALE + PULSE_RATIO * percentage;
+}
+
+
+/**
+ * @param {number[]} frequencies - A Wallpaper Engine audio frame
+ * @returns {number} Average value from the range of frequencies
+ */
+function biasedAverageLoudness(frequencies) {
+	let total = 0;
+	for (let i = START_VALUE; i < NB_VALUE + START_VALUE; i++) {
+		total += getFrequencyRatio(frequencies, i);
+	}
+	return sigmoid(total / NB_VALUE);
+}
+
+
+function detectBeat(frequencies) {
+	let result = false;
+	const loudness = biasedAverageLoudness(frequencies);
+	const timeSinceLastBeat = new Date().getTime() - lastBeatTime;
+	if (lastLoudness < THESHOLD_LOW && loudness >= THESHOLD_HIGH && timeSinceLastBeat > 250) {
+		lastBeatTime = new Date().getTime();
+		result = true;
+	}
+	lastLoudness = loudness;
+	return result;
 }
 
 
@@ -45,17 +88,8 @@ function analyse(frequencies) {
 }
 
 
-/**
- * @param {number[]} frequencies - A Wallpaper Engine audio frame
- * @param {number} startingValue - The index of the first frequency to average
- * @param {number} nbValue - The number of frequencies to average
- * @returns {number} Average value from the range of frequencies
- */
-export function getAverageLoudness(frequencies, startingValue, nbValue) {
-	analyse(frequencies);
-	let total = 0;
-	for (let i = startingValue; i < nbValue + startingValue; i++) {
-		total += getFrequencyRatio(i);
-	}
-	return total / nbValue;
-}
+export default {
+	analyse,
+	biasedAverageLoudness,
+	detectBeat,
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import { getAverageLoudness } from "./analyse.js";
-import { dance } from "./dance.js";
+// const readout = document.querySelector("#readout");
+// window.addEventListener("error", (error) => {
+// 	readout.textContent += error.message + "\n";
+// });
 
-const START_VALUE = 0;
-const NB_VALUE = 10;
+
+import analyser from "./analyse.js";
 
 const pix = document.querySelector(".pix");
 const bit = document.querySelector(".bit");
+
+
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+/**
+ * @param {HTMLElement} elem - The HTML element target to animate
+ * @todo: The true pattern is "crouch", "flip", "up", "down", then
+ *                            "crouch", "flip" AND "up", "down".
+ */
+async function go(elem) {
+	elem.style.transform = `scaleY(0.95)`;  // crouch
+	await delay(33);
+	elem.classList.toggle("go");  // flip
+	elem.style.transform = `scaleY(1.05)`;  // up
+	await delay(33)
+	elem.style.transform = "";  // down
+}
+
 
 /**
  * Process incoming audio.
@@ -13,7 +36,15 @@ const bit = document.querySelector(".bit");
  * @param {number[]} frequencies - A Wallpaper Engine audio frame
  */
 window.wallpaperRegisterAudioListener((frequencies) => {
-	const loudness = getAverageLoudness(frequencies, START_VALUE, NB_VALUE);
-	dance(pix, loudness);
-	dance(bit, loudness);
+	analyser.analyse(frequencies);
+	if (analyser.detectBeat(frequencies)) {
+		go(pix);
+		go(bit);
+	}
+
+	// const loudness = analyser.biasedAverageLoudness(frequencies);
+	// const thing = Math.round(10 * loudness);
+	// readout.textContent += "▮".repeat(thing) + "▯".repeat(10 - thing) + "\n";
+	// readout.scrollTop = readout.scrollHeight;
+	// if (readout.textContent.length > 10000) readout.textContent = "";
 });
